9f83d17a069d1925c58610cb9745d228709540d8
VladimirDTK <126583650+VladimirDTK@users.noreply.github.com>
2023-05-09 08:26:21 -0600

    Add files via upload

---
+++ Empleado_Examen/Empleado_Examen/Empleado.cs
@@ -0,0 +1,9 @@
+﻿class Empleado
+{
+    public int ID { get; set; }
+    public string Nombre { get; set; }
+    public string ApellidoPaterno { get; set; }
+    public string ApellidoMaterno { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public bool esContratado { get; set; }
+}
---
+++ Empleado_Examen/Empleado_Examen/Program.cs
@@ -0,0 +1,75 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido al programa de nóminas");
+        Console.WriteLine("¿Qué tipo de empleado es usted?");
+        Console.WriteLine("1. Contratado");
+        Console.WriteLine("2. A destajo");
+
+        int opcion = int.Parse(Console.ReadLine());
+
+        Empleado empleado;
+
+        Console.WriteLine("Ingrese el ID del empleado:");
+        int id = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el nombre del empleado:");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el apellido paterno del empleado:");
+        string apellidoPaterno = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el apellido materno del empleado:");
+        string apellidoMaterno = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la fecha de ingreso del empleado (DD/MM/AAAA):");
+        DateTime fechaIngreso = DateTime.Parse(Console.ReadLine());
+
+        if (opcion == 1)
+        {
+            Console.WriteLine("Ingrese los años de experiencia del empleado:");
+            int aniosExperiencia = int.Parse(Console.ReadLine());
+
+            empleado = new Empleado_Contratado
+            {
+                ID = id,
+                Nombre = nombre,
+                ApellidoPaterno = apellidoPaterno,
+                ApellidoMaterno = apellidoMaterno,
+                FechaIngreso = fechaIngreso,
+                esContratado = true,
+                AniosExperiencia = aniosExperiencia
+            };
+
+            double salario = ((Empleado_Contratado)empleado).CalcularSalario();
+            Console.WriteLine($"ID: {empleado.ID}");
+            Console.WriteLine($"Nombre: {empleado.Nombre} {empleado.ApellidoPaterno} {empleado.ApellidoMaterno}");
+            Console.WriteLine($"Fecha de ingreso: {empleado.FechaIngreso.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Salario: {salario:C}");
+        }
+        else
+        {
+            Console.WriteLine("Ingrese el número de clientes del empleado:");
+            int numClientes = int.Parse(Console.ReadLine());
+
+            empleado = new Empleado_Destajo
+            {
+                ID = id,
+                Nombre = nombre,
+                ApellidoPaterno = apellidoPaterno,
+                ApellidoMaterno = apellidoMaterno,
+                FechaIngreso = fechaIngreso,
+                esContratado = false,
+                NumClientes = numClientes
+            };
+
+            double pago = ((Empleado_Destajo)empleado).CalcularPago();
+            Console.WriteLine($"ID: {empleado.ID}");
+            Console.WriteLine($"Nombre: {empleado.Nombre} {empleado.ApellidoPaterno} {empleado.ApellidoMaterno}");
+            Console.WriteLine($"Fecha de ingreso: {empleado.FechaIngreso.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Pago: {pago:C}");
+        }
+    }
+}
+
