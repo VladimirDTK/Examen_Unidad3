c8f8c4368e90194f993d34054af51b6e233487c6
VladimirDTK <126583650+VladimirDTK@users.noreply.github.com>
2023-05-09 09:29:04 -0600

    Add files via upload

---
+++ Examen/Empleado_Contratado.cs
@@ -0,0 +1,27 @@
+﻿class Empleado_Contratado : Empleado
+{
+    public int AniosExperiencia { get; set; }
+
+    public double CalcularSalario()
+    {
+        double salarioFijo = 1000;
+        double porcentaje = 0;
+        if (AniosExperiencia <= 3)
+        {
+            porcentaje = 0.05;
+        }
+        else if (AniosExperiencia <= 7)
+        {
+            porcentaje = 0.1;
+        }
+        else if (AniosExperiencia <= 15)
+        {
+            porcentaje = 0.15;
+        }
+        else
+        {
+            porcentaje = 0.2;
+        }
+        return salarioFijo * (1 + porcentaje);
+    }
+}
---
+++ Examen/Empleado_Destajo.cs
@@ -0,0 +1,9 @@
+﻿class Empleado_Destajo : Empleado
+{
+    public int NumClientes { get; set; }
+
+    public double CalcularPago()
+    {
+        return NumClientes * 130;
+    }
+}
